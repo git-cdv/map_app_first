920c5790123e3e1e3509a6a5d57161f7ef5b37ea
chkan <chkan.dv@gmail.com>
2021-11-23 18:11:52 +0200

    + SearchView

--- app/src/main/java/com/chkan/firstproject/ui/FromFragment.kt
+++ app/src/main/java/com/chkan/firstproject/ui/FromFragment.kt
@@ -5,16 +5,57 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.chkan.firstproject.R
+import android.widget.ArrayAdapter
+import androidx.appcompat.widget.SearchView
+import com.chkan.firstproject.databinding.FragmentFromBinding
 
 class FromFragment : Fragment() {
+
+    private var _binding: FragmentFromBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_from, container, false)
+        _binding = FragmentFromBinding.inflate(inflater, container, false)
+
+        val user = arrayOf("Abhay","Joseph","Maria","Avni","Apoorva","Chris","David","Kaira","Dwayne","Christopher",
+            "Jim","Russel","Donald","Brack","Vladimir")
+
+        val userAdapter : ArrayAdapter<String> = ArrayAdapter(
+            context!!,
+            android.R.layout.simple_list_item_1,
+            user
+        )
+
+        binding.listFrom.adapter = userAdapter
+
+        binding.searchFrom.setOnQueryTextListener(object  : SearchView.OnQueryTextListener{
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                binding.searchFrom.clearFocus()
+                if (user.contains(query)){
+
+                    userAdapter.filter.filter(query)
+
+                }
+                return false
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                userAdapter.filter.filter(newText)
+                return false
+            }
+
+
+        })
+
+
+        return binding.root
     }
 
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
--- app/src/main/java/com/chkan/firstproject/ui/ToFragment.kt
+++ app/src/main/java/com/chkan/firstproject/ui/ToFragment.kt
@@ -5,15 +5,57 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import androidx.appcompat.widget.SearchView
 import com.chkan.firstproject.R
+import com.chkan.firstproject.databinding.FragmentFromBinding
+import com.chkan.firstproject.databinding.FragmentToBinding
 
 class ToFragment : Fragment() {
+
+    private var _binding: FragmentToBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_to, container, false)
+        _binding = FragmentToBinding.inflate(inflater, container, false)
+
+        val user = arrayOf("Abhay","Joseph","Maria","Avni","Apoorva","Chris","David","Kaira","Dwayne","Christopher",
+            "Jim","Russel","Donald","Brack","Vladimir")
+
+        val userAdapter : ArrayAdapter<String> = ArrayAdapter(
+            context!!,
+            android.R.layout.simple_list_item_1,
+            user
+        )
+
+        binding.listTo.adapter = userAdapter
+
+        binding.searchTo.setOnQueryTextListener(object  : SearchView.OnQueryTextListener{
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                binding.searchTo.clearFocus()
+                if (user.contains(query)){
+
+                    userAdapter.filter.filter(query)
+
+                }
+                return false
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                userAdapter.filter.filter(newText)
+                return false
+            }
+
+        })
+
+        return binding.root
     }
 
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
