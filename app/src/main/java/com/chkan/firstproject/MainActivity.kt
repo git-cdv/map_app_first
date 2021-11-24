25e04e0f4b8fd95970bf342c3ce7a35b4939407a
chkan <chkan.dv@gmail.com>
2021-11-24 09:34:47 +0200

    + Design + Map

--- app/src/main/java/com/chkan/firstproject/MainActivity.kt
+++ app/src/main/java/com/chkan/firstproject/MainActivity.kt
@@ -2,6 +2,7 @@ package com.chkan.firstproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.chkan.firstproject.databinding.ActivityMainBinding
 import com.chkan.firstproject.ui.ViewPagerAdapter
 import com.google.android.material.tabs.TabLayoutMediator
@@ -9,6 +10,7 @@ import com.google.android.material.tabs.TabLayoutMediator
 class MainActivity : AppCompatActivity() {
 
     lateinit var binding: ActivityMainBinding
+    private var back_pressed: Long = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,7 +20,7 @@ class MainActivity : AppCompatActivity() {
 
         val viewPager = binding.viewPager
         val tabLayout = binding.tabLayout
-
+        viewPager.isUserInputEnabled = false
         val adapter = ViewPagerAdapter(supportFragmentManager, lifecycle)
         viewPager.adapter = adapter
 
@@ -27,4 +29,13 @@ class MainActivity : AppCompatActivity() {
             else {tab.text = "Куда"}
         }.attach()
     }
+
+    override fun onBackPressed() {
+        if (back_pressed + 2000 > System.currentTimeMillis()) super.onBackPressed() else Toast.makeText(
+            baseContext, "Для выхода нажмите \"назад\" еще раз",
+            Toast.LENGTH_SHORT
+        ).show()
+        back_pressed = System.currentTimeMillis()
+    }
+
 }
--- app/src/main/java/com/chkan/firstproject/ui/FromFragment.kt
+++ app/src/main/java/com/chkan/firstproject/ui/FromFragment.kt
@@ -7,10 +7,19 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import androidx.appcompat.widget.SearchView
+import com.chkan.firstproject.R
 import com.chkan.firstproject.databinding.FragmentFromBinding
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import java.util.*
 
 class FromFragment : Fragment() {
 
+    private var mapFragment: SupportMapFragment? = null
     private var _binding: FragmentFromBinding? = null
     private val binding get() = _binding!!
 
@@ -29,7 +38,7 @@ class FromFragment : Fragment() {
             user
         )
 
-        binding.listFrom.adapter = userAdapter
+        //binding.listFrom.adapter = userAdapter
 
         binding.searchFrom.setOnQueryTextListener(object  : SearchView.OnQueryTextListener{
             override fun onQueryTextSubmit(query: String?): Boolean {
@@ -46,16 +55,52 @@ class FromFragment : Fragment() {
                 userAdapter.filter.filter(newText)
                 return false
             }
-
-
         })
 
+        if (mapFragment == null) {
+            mapFragment = SupportMapFragment.newInstance()
+            mapFragment!!.getMapAsync(OnMapReadyCallback { map ->
+
+                // добавляем маркер по координатам и "фокусируемся" на нем
+                val latLngWork = LatLng(47.84303067630826, 35.13851845689717)
+                val zoomLevel = 15f
+                map.moveCamera(CameraUpdateFactory.newLatLngZoom(latLngWork, zoomLevel))
+
+                setMapLongClick(map)//сетим слушатель на лонгклик для маркера
+
+            })
+        }
+
+        // R.id.map is a FrameLayout, not a Fragment
+        childFragmentManager.beginTransaction().replace(R.id.map, mapFragment!!).commit()
 
         return binding.root
+    }
+
+    //создаем маркер при долгом нажатии
+    private fun setMapLongClick(map: GoogleMap) {
+        map.setOnMapLongClickListener { latLng ->
+
+            // A snippet is additional text that's displayed after the title.
+            val snippet = String.format(
+                Locale.getDefault(),
+                "Lat: %1$.5f, Long: %2$.5f",
+                latLng.latitude,
+                latLng.longitude
+            )
+
+            map.addMarker(
+                MarkerOptions()
+                    .position(latLng)
+                    .title(getString(R.string.dropped_pin))//тайтл для снипета
+                    .snippet(snippet)//текст для снипета
+            )
+        }
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
     }
+
 }
