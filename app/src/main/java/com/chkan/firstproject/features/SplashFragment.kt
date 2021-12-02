08bde3ac5349d031b3599afcd0850f7f38e20a24
chkan <chkan.dv@gmail.com>
2021-12-02 11:42:30 +0200

    + Navigation: Splash -> MainFragment

--- app/src/main/java/com/chkan/firstproject/features/SplashFragment.kt
+++ app/src/main/java/com/chkan/firstproject/features/SplashFragment.kt
@@ -1,55 +1,29 @@
 package com.chkan.firstproject.features
 
-import android.content.Intent
-import android.os.Build
 import android.os.Bundle
-import android.util.Log
 import android.view.*
 import androidx.fragment.app.Fragment
-import com.chkan.firstproject.MainActivity
+import androidx.navigation.fragment.findNavController
 import com.chkan.firstproject.R
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.*
 
 class SplashFragment : Fragment() {
+
+    private val scope = CoroutineScope(Dispatchers.Default)
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_splash, container, false)
     }
 
+    override fun onStart() {
+        super.onStart()
+        scope.launch {
+            delay(1000L)
+            withContext(Dispatchers.Main){findNavController().navigate(R.id.action_splashFragment_to_mainFragment)}
+        }
+    }
 }
 
-/*
-private val scope = CoroutineScope(Dispatchers.Default)
-
-override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(savedInstanceState)
-    setContentView(R.layout.activity_splash)
-
-    val intent = Intent(this, MainActivity::class.java)
-
-    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
-        window.insetsController?.hide(WindowInsets.Type.statusBars())
-    } else {
-        window.setFlags(
-            WindowManager.LayoutParams.FLAG_FULLSCREEN,
-            WindowManager.LayoutParams.FLAG_FULLSCREEN
-        )
-    }
-    delayAndToMain(intent)
-}
-
-private fun delayAndToMain(intent: Intent) {
-    Log.d("MYAPP", "SplashActivity")
-    scope.launch {
-        delay(1000L)
-        startActivity(intent)
-    }
-
-}*/
